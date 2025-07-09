3432f786e234835fa9cf33548c88040e7485594d
soniya-kothari <soniya.kothari116325@marwadiuniversity.ac.in>
2025-07-09 22:00:58 +0530

    Update todo.py

--- todo.py
+++ todo.py
@@ -7,7 +7,7 @@ def show_tasks():
         with open(FILENAME, "r") as file:
             tasks = file.readlines()
         if not tasks:
-            print("📭 No tasks found."
+            pri("📭 No tasks found.")
         else:
             print("\n📋 Your To-Do List:")
             for idx, task in enumerate(tasks, 1):
